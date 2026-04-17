ba549036a7d113aedd5f26709fb692dc8b6f2966
MadisonShook3 <ms6377@engr.ship.edu>
2026-04-17 01:01:17 -0400

    Tests 1-6 passing with commented, refactored code.

--- wzip.c
+++ wzip.c
@@ -5,45 +5,50 @@ int prev_char;
 int count;
 
 int main(int argc, char *argv[]) {
-	if(argc <= 1) {
-		printf("wzip: file1 [file2 ...]\n");
-		return 1;
-	}
+   if(argc <= 1) {    //if no file after ./wzip
+        printf("wzip: file1 [file2 ...]\n");
+        return 1;
+    }
 
-	for(int i = 1; i < argc; i++) {
-		FILE *file = fopen(argv[i], "r");
-		if(file == NULL) {
-        	printf("Error opening file");
-        	return 1;
-    	} else if(i == 1) {
-			setup_file(file);
-		}
-    	parse_input(file);
-		fclose(file);		
-	}
-	fwrite(&count, sizeof(int), 1, stdout);
-    fwrite(&prev_char, sizeof(char), 1, stdout);	
-	
-	return 0;
+    for(int i = 1; i < argc; i++) {  //loop through arguments
+        FILE *file = fopen(argv[i], "r"); //open file
+        if(file == NULL) {
+            printf("Error opening file");
+            return 1;
+        } else if(i == 1) {    //if just started on first file, 
+                               //need to give the global vars values
+            setup_file(file);
+        }
+        parse_input(file);
+        fclose(file);       
+    }
+    fwrite(&count, sizeof(int), 1, stdout);    //write the value
+    fwrite(&prev_char, sizeof(char), 1, stdout);   
+    //write the character after the value
+    
+    return 0;
 }
 
 void parse_input(FILE *file) {
-	int new_char;
-	while((new_char = fgetc(file)) != EOF) {
-		if(new_char == prev_char) {
-			count = count + 1;
-		} else {
-			fwrite(&count, sizeof(int), 1, stdout);
-			fwrite(&prev_char, sizeof(char), 1, stdout);
-			prev_char = new_char;
-			count = 1;
-			continue;
-		}
-	}
+    int new_char;
+    while((new_char = fgetc(file)) != EOF) {
+        if(new_char == prev_char) {  
+        //if chars are the same, keep count
+            count = count + 1;
+        } else {
+            fwrite(&count, sizeof(int), 1, stdout);
+            fwrite(&prev_char, sizeof(char), 1, stdout);
+            prev_char = new_char;
+            count = 1;
+            continue;
+        }
+    }
 }
 
+//just gives the global variables their starting 
+//values and if its the end of file, return.
 void setup_file(FILE *file) {
-	prev_char = fgetc(file);
+    prev_char = fgetc(file);
     if(prev_char == EOF) return;
     count = 1;
 }
